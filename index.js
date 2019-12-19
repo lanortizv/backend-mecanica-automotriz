c679c1fa073fd14dd595a552c421ce47b2c9f7d0
Alan Ortiz <alandaviov@gmail.com>
2019-12-19 08:58:17 -0400

    module exports fixed

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { pool } = require('./config')
-const methods=require('./methods')
 const app = express()
-require("@babel/register")({
-    presets: ["@babel/preset-env"]
-  });
+const controller = require('./controller')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-
-
 app
   .route('/clientes')
   // GET endpoint
-  .get(methods.getClients)
+  .get(controller.getClients)
   // POST endpoint
-  .post(methods.addClient)
+  .post(controller.addClient)
 
 // Start server
-app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening`)
+const port = process.env.PORT || 3002
+app.listen(port, () => {
+  console.log(`Server listening at `+port)
 })
